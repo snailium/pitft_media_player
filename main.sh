7eaed992e4da4c2fa9c87a244dcb86ba17f4a766
Guannan Wang <snailium@hotmail.com>
2018-09-14 04:35:45 +0000

    Completed the menus.

--- main.sh
+++ main.sh
@@ -3,12 +3,16 @@
 MEDIA_ROOT=~/Media
 MEDIA_DIR=.
 
+WALLPAPER=/home/pi/grumpy-cat.png
+
+MENU_DEFAULT=
+
 SELECTION=menu.sh.$$
 
 function list_media {
 
   DIR_WORKING=${MEDIA_ROOT}/${MEDIA_DIR}
-  DIR_LIST=$(ls -1 ${DIR_WORKING} | perl -p -e 's/^(.*)\n$/$1 "s" /g')
+  DIR_LIST=$(ls -1 ${DIR_WORKING} | perl -p -e 's/^(.*)\n$/$1 $1 /g')
   DIR_UP=
 
   if [ -z ${MEDIA_DIR} ]; then
@@ -19,11 +23,22 @@ function list_media {
     rm -rf ${SELECTION}
   fi
 
-  dialog --clear --backtitle "Ultra Super Media Management" \
-  --title "[ ${MEDIA_DIR} ]" \
-  --menu "${DIR_WORKING} \n\
-  Choose the item" 15 50 10 \
-  ${DIR_LIST}  2>"${SELECTION}"
+#  sudo killall fbi
+#  sudo fbi -a --noverbose -T 2 ${WALLPAPER} &
+
+  DEFAULT_ITEM=
+  if [ ! -z ${MENU_DEFAULT} ]; then
+    DEFAULT_ITEM="--default-item ${MENU_DEFAULT}"
+  fi
+
+  dialog --clear \
+  --backtitle "Ultra Super Media Management" \
+  --cancel-label "Exit" \
+  --no-tags --no-shadow \
+  ${DEFAULT_ITEM} \
+  --title "[ VIDEO PLAYER ]" \
+  --menu "${MEDIA_DIR}" 25 40 20 \
+  ${DIR_LIST} 2>"${SELECTION}"
 
 
 
@@ -33,25 +48,53 @@ function list_media {
 #  Choose the item" 15 50 4 \
 #  ${DIR_LIST}  2>"${SELECTION}"
 
+  RETURN=$?
   RESULT=$(<"${SELECTION}")
 
-  echo ${RESULT}
- # echo ${DIR_LIST}
+  case $RETURN in
+    0)
+      # Valid selection
+      ITEM=${DIR_WORKING}/${RESULT}
+      echo ${ITEM}
+      if [ -f "${ITEM}" ]; then
+        omxplayer -o hdmi ${ITEM}
+        MENU_DEFAULT=${RESULT}
+        list_media
+      fi
+      if [ -d "${ITEM}" ]; then
+        MEDIA_DIR=${MEDIA_DIR}/${RESULT}
+        MENU_DEFAULT=
+        list_media
+      fi
+      ;;
 
-  if [ -z ${RESULT} ]; then
-    exit
-  fi
+    1)
+      # Pressed "Cancel"
+      gracefully_exit
+      if [ ${MEDIA_DIR} != ${MEDIA_ROOT} ]; then
+        MEDIA_DIR=$(dirname ${MEDIA_DIR})
+      fi
+      MENU_DEFAULT=
+      list_media
+      ;;
 
-  ITEM=${DIR_WORKING}/${RESULT}
-  echo ${ITEM}
-  if [ -f "${ITEM}" ]; then
-    omxplayer -o hdmi ${ITEM}
-    list_media
+    255)
+      # Pressed "ESC"
+      if [ ${MEDIA_DIR} != ${MEDIA_ROOT} ]; then
+        MENU_DEFAULT=$(basename "${MEDIA_DIR}")
+        MEDIA_DIR=$(dirname "${MEDIA_DIR}")
+      fi
+      list_media
+      ;;
+
+  esac
+}
+
+function gracefully_exit {
+  if [ -e ${SELECTION} ]; then
+    rm -rf ${SELECTION}
   fi
-  if [ -d "${ITEM}" ]; then
-    MEDIA_DIR=${MEDIA_DIR}/${RESULT}
-    list_media
-  fi
+  exit
 }
 
 list_media
